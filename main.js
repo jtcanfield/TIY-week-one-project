77787dd80507bdf8647ff55058edd98cdcb244ba
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-21 19:50:34 -0400

    commented some more JS

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+// function main() {
+//   alert("js on");
+//     $("#textdiv1").on({
+//         hover:function(){
+//             $(this).css({
+//                 left:(Math.random()*50)+"px",
+//                 top:(Math.random()*50)+"px",
+//             });
+//         }
+//     });
+// }
+// $(document).ready(main);
 /* im definately not about to do some js stuff
-<script src='/main.js'></script>
-<script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
+
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="main.js"></script>
 
 This belongs in footer under Copyright:
 <p id="clickme">click me...</p>
@@ -16,3 +30,4 @@ This Belongs in CSS
 #clickme:hover {
   opacity: 1;
 }
+*/
